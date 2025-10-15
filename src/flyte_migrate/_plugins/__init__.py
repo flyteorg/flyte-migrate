188b12017b2380f30dc92906a20de9f2afcb7b52
machichima <nary12321@gmail.com>
2025-10-15 09:58:54 +0800

    fix: directly return if v1_config is None
    Signed-off-by: machichima <nary12321@gmail.com>

--- src/flyte_migrate/_plugins/__init__.py
+++ src/flyte_migrate/_plugins/__init__.py
@@ -5,6 +5,9 @@ from flyte_migrate._plugins.ray import _transform_ray_config_v1_to_v2
 
 
 def _transform_plugin_config_v1_to_v2(v1_config: Optional[Any]) -> Optional[Any]:
+    if v1_config is None:
+        return None
+
     cfg = None
     trans = [_transform_spark_config_v1_to_v2, _transform_ray_config_v1_to_v2]
     for tran in trans:
