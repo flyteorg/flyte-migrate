2f17e47cf65e827f438960ec210856a4e0d5933c
yuteng <a08h0283@gmail.com>
2025-10-22 07:31:42 +0800

    set inputs for plan
    Signed-off-by: yuteng <a08h0283@gmail.com>

--- src/flyte_migrate/_launchplan.py
+++ src/flyte_migrate/_launchplan.py
@@ -11,24 +11,41 @@ from flytekit.core import workflow as _annotated_workflow
 from flytekit.models import common as _common_models
 from flytekit.models import schedule as _schedule_model
 
-def Schedule_to_trigger(
-    schedule: Optional[_schedule_model.Schedule] = None
+def merge_inputs(
+    default_inputs: Optional[Dict[str, Any]] = None,
+    fixed_inputs: Optional[Dict[str, Any]] = None,
+) -> dict:
+    if default_inputs is None and fixed_inputs is None:
+        return {}
+    if fixed_inputs is None:
+        return default_inputs
+    if default_inputs is None:
+        return fixed_inputs
+    return {**default_inputs, **fixed_inputs}
+
+def schedule_to_trigger(
+    schedule: Optional[_schedule_model.Schedule] = None,
+    default_inputs: Optional[Dict[str, Any]] = None,
+    fixed_inputs: Optional[Dict[str, Any]] = None,
 ) -> Optional[Trigger]:
     if schedule is None:
         return None
-    # Is Cron
+    inputs = merge_inputs(default_inputs, fixed_inputs)
     if schedule.rate is not None:
         return Trigger(
             name="my_fixed_rate_trigger",
             automation=FixedRate(schedule.rate.value),
+            default_inputs=inputs,
         )
     elif schedule.cron_expression is not None:
         return Trigger(
             name="my_cron_trigger",
             automation=Cron(schedule.cron_expression),
+            default_inputs=inputs,
         )
     else:
         return None
+
 class launchPlan_transformer(object):
     @classmethod
     def get_or_create(
