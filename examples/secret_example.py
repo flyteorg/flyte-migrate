7c87e9fe23a53f424e6a6e9db214d76920547b70
Alex Chien <115371007+yuhuan130@users.noreply.github.com>
2025-11-08 03:05:46 +0800

    secret_transformation_v1_v2 (#5)
    Signed-off-by: Alex Chien <alexchien130@gmail.com>

---
+++ examples/secret_example.py
@@ -0,0 +1,35 @@
+import flyte_migrate
+from typing import Tuple
+from pathlib import Path
+import logging
+from flytekit import Secret, task, workflow
+import os
+
+@task(secret_requests=[Secret(group="", key="API_TOKEN", env_var="API_TOKEN_ENV")])
+def use_secret_env() -> str:
+    val = os.getenv("API_TOKEN_ENV") 
+    try:
+        print("FILE being created ?", os.listdir(Path("/etc/flyte/secrets")))
+    except FileNotFoundError:
+        print(f"Didn't mount a File")
+    return f"ENV secret: {val}"
+
+@task(secret_requests=[Secret(group="", key="API_TOKEN", mount_requirement=Secret.MountType.FILE)])
+def use_secret_file() -> str:
+    path = Path("/etc/flyte/secrets")
+    if os.path.isdir(path):
+        print("FILE being created ?", os.listdir(path))
+    with open(f"{path}/API_TOKEN") as f:
+        val = f.read().strip()
+    return f"FILE secret: {val}"
+
+@workflow
+def wf(name: str) ->Tuple[str, str]:
+    return (use_secret_env(), use_secret_file())
+
+if __name__ == "__main__":
+    import flyte
+    flyte.init_from_config(log_level=logging.DEBUG)
+    run = flyte.with_runcontext(mode="remote", log_level=logging.DEBUG).run(wf, name="flyte")
+    print(run.name)
+    print(run.url)
--- src/flyte_migrate/_secret.py
+++ src/flyte_migrate/_secret.py
@@ -1,8 +1,21 @@
-from typing import List, Optional
-
+from typing import List, Optional, Any
+from pathlib import Path
 import flyte
 import flytekit
 
-
 def _transform_secret_v1_to_v2(secret_requests: Optional[List[flytekit.Secret]]) -> Optional[flyte.SecretRequest]:
-    return None
+    if not secret_requests:
+        return None
+    out: List[flyte.Secret] = []
+    for s in secret_requests:
+        out.append(
+            flyte.Secret(
+                key=s.key,
+                group=s.group,
+                as_env_var=s.env_var \
+                    if s.mount_requirement != flytekit.Secret.MountType.FILE else None,
+                mount=s.env_var \
+                    if s.mount_requirement == flytekit.Secret.MountType.FILE else None
+            )
+        )
+    return out
