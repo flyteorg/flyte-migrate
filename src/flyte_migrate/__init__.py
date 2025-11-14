c98aa0fc1253f0fed56582d78ddcbd3919fd4cb7
Alex Chien <115371007+yuhuan130@users.noreply.github.com>
2025-11-15 04:25:26 +0800

    Transform v1 deck to v2 report (#10)
    Signed-off-by: Alex Chien <alexchien130@gmail.com>

--- src/flyte_migrate/__init__.py
+++ src/flyte_migrate/__init__.py
@@ -1,1 +1,1 @@
-from flyte_migrate import _context, _dynamic, _launchplan, _map, _task, _workflow  # noqa: F401
+from flyte_migrate import _context, _dynamic, _launchplan, _map, _task, _workflow, _deck_to_report  # noqa: F401
