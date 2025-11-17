a6e0cccb24bf1a89dfc8a4e62507b507ea3f2450
yuteng <a08h0283@gmail.com>
2025-11-17 16:20:59 +0800

    allow set remote file name
    Signed-off-by: yuteng <a08h0283@gmail.com>

--- src/flyte_migrate/_file.py
+++ src/flyte_migrate/_file.py
@@ -1,14 +1,16 @@
 import os
 import typing
+from contextlib import contextmanager
 from pathlib import Path
+from typing import IO, Any, Generator, Optional
 
 import flytekit
+from flyte._context import internal_ctx
 from flyte.io import File
+from flyte.io._hashing_io import HashMethod
 from flyte.types import TypeEngine, TypeTransformer, TypeTransformerFailedError
 from flyteidl2.core import literals_pb2, types_pb2
-from typing import Optional, Generator, IO, Any
-from contextlib import contextmanager
-from flyte.io._hashing_io import HashMethod
+
 
 def noop(): ...
 
@@ -16,7 +18,7 @@ def noop(): ...
 T = typing.TypeVar("T")
 
 
-class FlyteFileV1ToV2():
+class FlyteFileV1ToV2:
     _file: Optional[File] = None
     is_download: bool = False
     local_path: str = ""
@@ -25,35 +27,41 @@ class FlyteFileV1ToV2():
     def from_source(cls, source: str | os.PathLike) -> "FlyteFileV1ToV2":
         python_val = File.from_existing_remote(source)
         return cls(file=python_val)
-    
-    @classmethod
-    def new_remote_file(cls, hash_method: Optional[HashMethod | str] = None, **kwargs) -> "FlyteFileV1ToV2":
-        return cls(file=File.new_remote(hash_method=hash_method))
 
-    def __init__(
-        self,
-        file: Optional[File] = None,
-        **kwargs
-    ):
+    @classmethod
+    def new_remote_file(
+        cls, path: Optional[str | os.PathLike] = None, hash_method: Optional[HashMethod | str] = None, **kwargs
+    ) -> "FlyteFileV1ToV2":
+        # TODO
+        # file_name is not supported in File.new_remote
+        ctx = internal_ctx()
+        known_cache_key = hash_method if isinstance(hash_method, str) else None
+        method = hash_method if isinstance(hash_method, HashMethod) else None
+        file_name = os.path.basename(path) if path else None
+        return cls(
+            path=ctx.raw_data.get_random_remote_path(file_name=file_name), hash=known_cache_key, hash_method=method
+        )
+
+    def __init__(self, file: Optional[File] = None, **kwargs):
         if file:
             self._file = file
         else:
             f = File(**kwargs)
             self._file = f
-    
+
     @classmethod
     def new(cls, filename: str | os.PathLike) -> "FlyteFileV1ToV2":
         return cls(file=File(path=filename))
-    
+
     def download(self) -> str:
         return self.__fspath__()
-    
+
     def __fspath__(self) -> str:
         if not self.is_download:
             self.local_path = self._file.download_sync()
             self.is_download = True
         return self.local_path
-    
+
     @contextmanager
     def open(
         self,
@@ -62,7 +70,7 @@ class FlyteFileV1ToV2():
     ) -> Generator[IO[Any], None, None]:
         with self._file.open_sync(*args, **kwargs) as f:
             yield f
-    
+
     @property
     def path(self) -> str:
         return self._file.path
@@ -70,15 +78,15 @@ class FlyteFileV1ToV2():
     @property
     def name(self) -> str:
         return self._file.name
-    
+
     @property
     def format(self) -> str:
         return self._file.format
-    
+
     @property
     def hash(self) -> str:
         return self._file.hash
-    
+
     @property
     def hash_method(self):
         return self._file.hash_method
