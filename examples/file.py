6b0f1686355a695b1ba6401567be1044a55879ad
yuteng <a08h0283@gmail.com>
2025-11-30 17:45:49 +0800

    FlyteTypes examples
    Signed-off-by: yuteng <a08h0283@gmail.com>

--- examples/file.py
+++ examples/file.py
@@ -3,7 +3,12 @@ import flyte_migrate  # noqa: F401, I001
 import flytekit as fl
 import pandas as pd
 import os
+from dataclasses import dataclass
+import tempfile
 
+@dataclass
+class FlyteTypes:
+    file: fl.FlyteFile
 
 image = fl.ImageSpec(packages=["pandas"])
 
@@ -15,6 +20,20 @@ def download_file(ff: fl.FlyteFile):
     df = pd.read_csv(local_path)
     print(df)
 
+@fl.task(container_image=image)
+def upload_data() -> FlyteTypes:
+    file_path = tempfile.NamedTemporaryFile(delete=False)
+    file_path.write(b"Hello, World!")
+
+    fs = FlyteTypes(
+        file=fl.FlyteFile(file_path.name),
+    )
+    return fs
+
+@fl.task(container_image=image)
+def download_data(res: FlyteTypes):
+    f = open(res.file, "r")
+    assert f.read() == "Hello, World!"
 
 @fl.task(container_image=image)
 def load_csv(uri: str = "https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv") -> fl.FlyteFile:
@@ -33,6 +52,8 @@ def remove_some_rows(ff: fl.FlyteFile) -> fl.FlyteFile:
 
 @fl.workflow()
 def wf():
-    f = load_csv()
-    f2 = remove_some_rows(f)
-    download_file(f2)
+    o1 = load_csv()
+    o2 = remove_some_rows(ff=o1)
+    download_file(ff=o2)
+    o3 = upload_data()
+    download_data(res=o3)
