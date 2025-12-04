306c872927dbd3036255aba66326c9bbb4d9c94c
Nary Yeh <60069744+machichima@users.noreply.github.com>
2025-12-04 00:44:37 -0800

    [Improvement]: add linter and fmt in make and ci (#14)
    * build: add linter and fmt in make and ci
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    * build: add note in ci
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    * refactor: make fmt
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    * fix: mypy error
    
    Signed-off-by: machichima <nary12321@gmail.com>
    
    ---------
    
    Signed-off-by: machichima <nary12321@gmail.com>

--- examples/secret_example.py
+++ examples/secret_example.py
@@ -1,18 +1,20 @@
-import flyte_migrate
-from typing import Tuple
-from pathlib import Path
 import logging
-from flytekit import Secret, task, workflow
 import os
+from pathlib import Path
+from typing import Tuple
+
+from flytekit import Secret, task, workflow
+
 
 @task(secret_requests=[Secret(group="", key="API_TOKEN", env_var="API_TOKEN_ENV")])
 def use_secret_env() -> str:
-    val = os.getenv("API_TOKEN_ENV") 
+    val = os.getenv("API_TOKEN_ENV")
     try:
         print("FILE being created ?", os.listdir(Path("/etc/flyte/secrets")))
     except FileNotFoundError:
-        print(f"Didn't mount a File")
+        print("Didn't mount a File")
     return f"ENV secret: {val}"
+
 
 @task(secret_requests=[Secret(group="", key="API_TOKEN", mount_requirement=Secret.MountType.FILE)])
 def use_secret_file() -> str:
@@ -23,12 +25,15 @@ def use_secret_file() -> str:
         val = f.read().strip()
     return f"FILE secret: {val}"
 
+
 @workflow
-def wf(name: str) ->Tuple[str, str]:
+def wf(name: str) -> Tuple[str, str]:
     return (use_secret_env(), use_secret_file())
+
 
 if __name__ == "__main__":
     import flyte
+
     flyte.init_from_config(log_level=logging.DEBUG)
     run = flyte.with_runcontext(mode="remote", log_level=logging.DEBUG).run(wf, name="flyte")
     print(run.name)
