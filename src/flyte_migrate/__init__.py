7bfca3b9bb18c2f7d2b0f04744d06bc0c1fad326
yuteng <a08h0283@gmail.com>
2025-12-06 08:48:56 +0800

    fmt
    Signed-off-by: yuteng <a08h0283@gmail.com>

--- src/flyte_migrate/__init__.py
+++ src/flyte_migrate/__init__.py
@@ -1,1 +1,1 @@
-from flyte_migrate import _context, _dynamic, _file, _launchplan, _map, _task, _workflow, _deck_to_report  # noqa: F401
+from flyte_migrate import _context, _deck_to_report, _dynamic, _file, _launchplan, _map, _task, _workflow  # noqa: F401
