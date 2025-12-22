fdd5483ba6e705af235a750376b9ff5af9b6eb92
Kevin Su <pingsutw@apache.org>
2025-12-22 14:37:23 -0800

    Update src/flyte_migrate/_plugins/pytorch.py
    Co-authored-by: Nary Yeh <60069744+machichima@users.noreply.github.com>

--- src/flyte_migrate/_plugins/pytorch.py
+++ src/flyte_migrate/_plugins/pytorch.py
@@ -16,7 +16,9 @@ def _transform_pytorch_config_v1_to_v2(v1_config: Optional[Any]) -> Optional[Any
     run_policy = None
     if v1_config.run_policy is not None and v1_config.run_policy.clean_pod_policy is not None:
         run_policy = v2RunPolicy(
-            # flytesdk will capitalize the enum names, so we access the name attribute
+            # NOTE: While V2 expects lowercase (e.g., 'all'), we can use V1 Enums (e.g., 'ALL') here 
+            # as flyte-sdk capitalizes the policy value at runtime.
+            # Ref: https://github.com/flyteorg/flyte-sdk/blob/2863eb460ac31cf05bab994346c4221a143ba38c/plugins/pytorch/src/flyteplugins/pytorch/task.py#L171
             clean_pod_policy=v1_config.run_policy.clean_pod_policy.name,
             ttl_seconds_after_finished=v1_config.run_policy.ttl_seconds_after_finished,
             active_deadline_seconds=v1_config.run_policy.active_deadline_seconds,
